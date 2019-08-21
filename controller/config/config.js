7f23af1ece51b554b5c0ce1529cc8edd40ed8000
Fabrício Borgatto <fabricio.borgatto@gmail.com>
2019-08-21 09:08:34 -0300

    ajuste config

---
+++ controller/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	grantType: "client_credentials",
+	clientId: "21c9c8b5-c41d-452d-b325-0f6ecd32dfeb",
+	clientSecret: "af076c9b-fc2f-4e0a-80f0-9385700cf811",
+	url: "http://apigateway:8080/oauth2/token",
+	mongoUser: "hashtagApp",
+	mongoPass: "ajS3Lr0u1FBHzvZD",
+	mongoCluster: "cluster0-q7xvl.mongodb.net/test?retryWrites=true&w=majority",
+	mongoUrl: "mongodb://mongo:27017"
+}
